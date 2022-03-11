b821f8f1a7b7e594331e68ade3d16852cfd89399
Elan Thangamani <elans3.java@gmail.com>
2022-03-10 22:55:51 -0600

    Disable springdoc swagger-ui query params

--- src/main/java/io/virtualan/DisableSpringDocQueryParameter.java
+++ src/main/java/io/virtualan/DisableSpringDocQueryParameter.java
@@ -2,6 +2,7 @@ package io.virtualan;
 
 
 import javax.annotation.PostConstruct;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class DisableSpringDocQueryParameter {
 
   @PostConstruct
-  @Order(11111)
+  @Order(Ordered.LOWEST_PRECEDENCE-1 )
   private void load(){
     System.setProperty("springdoc.swagger-ui.queryConfigEnabled","false");
   }
